57c0fee449960d997e1dcc2837f7f76693db4706
Paul Brattain <arachane@gmail.com>
2017-04-27 18:55:52 -0700

    Adding to defect guide

--- defects.js
+++ defects.js
@@ -1,16 +1,47 @@
 var defect = document.querySelector("select");
-
+var remedyList = document.querySelector("#remedy");
+var causeList = document.querySelector("#cause");
+var displayCause = [];
+var displayRemedy = [];
 
 
 defect.addEventListener("input", function(event) {
+  while (causeList.firstChild){
+    causeList.removeChild(causeList.firstChild);
+  };
+  while (remedyList.firstChild){
+    remedyList.removeChild(remedyList.firstChild);
+  };
   //once defect is selected, add cause and remedy to page-heading
   var chosenDefect = (this.value);
-  console.log(chosenDefect);
-  console.log(chosenDefect.cause);
+  //iterate through defect array, find correct defect
+  for(i=0;i<defectType.length;i++) {
+    if (chosenDefect == defectType[i].type) {
+      displayCause = defectType[i].cause;
+      displayRemedy = defectType[i].remedy;
+      //append all causes to cause ul
+      for(i=0;i<displayCause.length;i++){
+        $("#cause").append("<li>" + displayCause[i] + "</li>");
+      };
+      //append all remedies to remedy ul
+      for(i=0;i<displayRemedy.length;i++){
+        $("#remedy").append("<li>" + displayRemedy[i] + "</li>");
+      };
+    }
+  }
 });
 
-// TODO  make the defect part of each object, iterate through the objects, match to type, return cause / remedy
-var highAa = {
-  cause: "Incorrect profile for Low AA level",
-  remedy: ["Reduce Machine Heats", "Reduce Mold Temperature", "Reduce Screw RPM (shear heat)", "Reduce Injection Fill Rate", "Reduce Transfer Rate", "Minimize Screw Cushion 10-20mm", "Minimize Residence Time", "Remove Barrel Head Filter", "Q.A. Check Material I.V>"]
-};
+//reset the page for new defect selection
+
+
+var defectType = [{
+  type: "highAa",
+  cause: ["Incorrect profile for Low AA level"],
+  remedy: ["Reduce Machine Heats", "Reduce Mold Temperature", "Reduce Screw RPM (shear heat)", "Reduce Injection Fill Rate", "Reduce Transfer Rate", "Minimize Screw Cushion 10-20mm", "Minimize Residence Time", "Remove Barrel Head Filter", "Q.A. Check Material I.V"]
+},
+{
+  type: "bubbles",
+  cause: ["(A) Incorrect Profile Set Up","(B) Melt too cold", "(C) Wet Material - Shutdown press and correct", "(D) Trapped air / gasses", "(E) Bad Resin", "(F) Damaged extruder check valve"],
+  remedy: ["(A) Check Master Process Sheet", "(A) Transfer Cushion too high, reduce cushion to 10-20mm", "(B) Barrel heats too low - Increase barrel heats", "(B) Screw RPM too low - Increase RPM", "(B) Increase back pressure", "(C) Check dryer temperature is at setpoint", "(C) Check for low level issue reducing residence time (prior loading faults)", "(C) Check dryer dew point", "(C) Check for proper residence time", "(C) Check for proper air flow in dryer", "(D) Increase screw back pressure", "(D) Increase or Decrease screw RPM", "(D) Increase or Decrease drying time / temperature", "(D) Gasses trapped in compression zone of screw - Lower heats at feed throat", "(E) Q.A. check I.V.", "(E) Contact MPT, Supervisor, Material Manager", "(F) Contact Maintenance Manager", "(F) Check screw / check valve"]
+}
+];
